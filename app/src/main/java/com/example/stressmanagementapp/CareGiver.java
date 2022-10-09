1ce433db5ab56d4cc93ac599a6ca9d5a338f4b6a
RheaAdh <rheadhikari@gmail.com>
2022-10-09 19:20:53 +0530

    Add Class Files

---
+++ app/src/main/java/com/example/stressmanagementapp/CareGiver.java
@@ -0,0 +1,19 @@
+package com.example.stressmanagementapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CareGiver extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.caregiver);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
---
+++ app/src/main/java/com/example/stressmanagementapp/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.stressmanagementapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+        Button btnSubmit = findViewById(R.id.btnLogin);
+        btnSubmit.setOnClickListener(view -> {
+            EditText etName = findViewById(R.id.et1);
+            EditText etPass = findViewById(R.id.et2);
+            String username = etName.getText().toString();
+            String password = etPass.getText().toString();
+
+            Toast.makeText(LoginActivity.this, username + "has logged in!", Toast.LENGTH_LONG).show();
+
+//            Intent intent = new Intent(LoginActivity.this, CareGiver.class);
+            Intent intent = new Intent(LoginActivity.this, Admin.class);
+            intent.putExtra("username", username);
+            startActivity(intent);
+        });
+    }
+}
